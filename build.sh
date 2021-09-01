ef98f17f7ef288bf44870e860299ce78ca3f0df0
Loïc Tosser <loic@kalvad.com>
2021-09-01 21:28:27 +0400

    build

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ if [[ -z "${GRAFANA_VERSION}" ]]; then
   exit 1
 fi
 
-wget" https://dl.grafana.com/enterprise/release/grafana-enterprise-${GRAFANA_VERSION}.linux-amd64.tar.gz"
+wget "https://dl.grafana.com/enterprise/release/grafana-enterprise-${GRAFANA_VERSION}.linux-amd64.tar.gz"
 tar xvzf grafana-enterprise-${GRAFANA_VERSION}.linux-amd64.tar.gz
